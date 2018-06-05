f080c96a851a047369b08fa146d67f7edefad7c8
QUAQUA <quaqua@QUAQUAdeMacBook-Air.local>
2018-06-05 10:09:15 +0800

    Initial Commit

---
+++ wave/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wave
+//
+//  Created by QUAQUA on 2018/6/5.
+//  Copyright © 2018年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wave/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wave
+//
+//  Created by QUAQUA on 2018/6/5.
+//  Copyright © 2018年 zws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
